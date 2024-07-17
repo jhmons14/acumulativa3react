4cca25ed3e844c8d7291545a96b5a76d68347295
jhmons14 <jho.vn1419@gmail.com>
2024-07-17 17:05:30 -0400

    tercer commit

--- src/componentes/PostItComponent.jsx
+++ src/componentes/PostItComponent.jsx
@@ -1,4 +1,6 @@
 import { useState, useRef } from "react";
+import { PostItSalida } from "./PostItSalida";
+import { v4 as uuid } from 'uuid';
 
 export function PostItComponent() {
 
@@ -7,13 +9,13 @@ export function PostItComponent() {
     const importanciaRef = useRef()
 
     const [postList, setPostList] = useState([
-        {title:'hola', description:'algo', important:true}
+        {id:uuid(), title:'hola', description:'algo', important:false}
     ])
     
     function agregarPostIt() {
         const titulo = tituloRef.current.value;
         const descripcion = descripcionRef.current.value;
-        const importancia = importanciaRef.current.checked;
+        const importancia = !importanciaRef.current.checked;
 
         
 
@@ -22,12 +24,21 @@ export function PostItComponent() {
         }
 
         const newPostIt = {
+            id: uuid(),
             title:titulo,
             description:descripcion,
             important:importancia
         }
         const newPostList = [...postList, newPostIt]
         console.log(newPostList);
+        setPostList(newPostList) //aca renderiza o actualiza cada vez que hace un cambio.
+    }
+
+    function eliminarPostIt(id) {
+        console.log('hola');
+        const postIts = postList.filter(post => post.id === id)
+        console.log(postIts);
+        setPostList(postIts)
     }
 
     return (
@@ -37,6 +48,10 @@ export function PostItComponent() {
             <input ref={descripcionRef} className="m-2" type="text" placeholder="Descripcion"/>
             <input ref={importanciaRef} type="checkbox" /><label htmlFor="">importante</label>
             <button className="btn btn-primary" onClick={agregarPostIt}>Agregar</button>
+            {/* <button onClick={eliminarPostIt}>x</button> */}
+            <div>
+                {postList.map(postIt => <PostItSalida eliminarPostIt={eliminarPostIt} postIt={postIt} key={postIt.id} />)}
+            </div>
         </div>
     )
 }
--- src/componentes/PostItSalida.jsx
+++ src/componentes/PostItSalida.jsx
@@ -1,8 +1,17 @@
-export function PostItSalida() {
+export function PostItSalida({postIt, eliminarPostIt}) {
+    // const eliminar = () => eliminarPostIt()
+    const {id, title, important, description } = postIt;
+    const eliminar = () => eliminarPostIt(id);
+
     return (
-        <div>
-            <h3>Titulo</h3>
-            <p>Description</p>
+        <div className="container .bg-danger">
+            <div>
+                <h3>{title}</h3>
+                <button onClick={eliminar}>X</button>
+                <p>{description}</p>
+            </div>
+            
+            
         </div>
     )
 }
